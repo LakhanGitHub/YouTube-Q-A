9e91c50767d5fc1798334a25315b146060f3c1ff
LakhanGitHub <109935532+LakhanGitHub@users.noreply.github.com>
2025-10-18 18:59:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    import pysqlite3 as sqlite3
+except ImportError:
+    import sqlite3
 
 
 import streamlit as st
